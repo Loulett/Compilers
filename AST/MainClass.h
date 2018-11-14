05de65de0f4a950108a7b8c2e10a184d37e151b7
Alexandra Iglina <aiglina@gmail.com>
2018-11-14 12:54:34 -0800

    Add unique_ptrs and errors log for MainClass

--- AST/MainClass.h
+++ AST/MainClass.h
@@ -1,12 +1,13 @@
 #pragma once
 #include "INode.h"
+#include <memory>
 
 class MainClass: public IMainClass {
  public:
 	MainClass(IIdentifier* class_name, IIdentifier* arg, IStatement* statement);
     void Accept(Visitor* v) const override;
  private:
- 	IIdentifier* class_name;
-	IIdentifier* arg;
-	IStatement* statement;
+ 	std::unique_ptr<IIdentifier> class_name;
+	std::unique_ptr<IIdentifier> arg;
+	std::unique_ptr<IStatement> statement;
 };
