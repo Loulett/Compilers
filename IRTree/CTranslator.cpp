a2ea697d6c7f9ea51dea2307fd419b6324bac01c
evgeniy <evgeniy.eg1@yandex.ru>
2019-03-02 00:03:48 +0300

    IRT, methods for translator

--- IRTree/CTranslator.cpp
+++ IRTree/CTranslator.cpp
@@ -3,7 +3,141 @@
 
 using namespace IRTree;
 
-void Translator::visit( const IdentifierExpression* n )
+//    MemoryExpression(BINOP(BINOP::PLUS, curFrame->FP(), ConstExpression()))
+
+void CTranslator::visit( const CGoal* n )
 {
-    MemoryExpression(BINOP(BINOP::PLUS, curFrame->FP(), ConstExpression()))
+    n->mainClass->Accept( this );
+
+    for( const auto& clazz : *n->classDeclarations ) {
+        clazz->Accept( this );
+    }
+}
+
+void CTranslator::visit( const CMainClass* n )
+{
+
+}
+
+void CTranslator::visit( const СClassDeclaration* n )
+{
+
+}
+
+void CTranslator::visit( const CVarDeclaration* n )
+{
+
+}
+
+void CTranslator::visit( const CArgument* n )
+{
+
+}
+
+void CTranslator::visit( const CMethodDeclaration* n )
+{
+
+}
+
+
+void CTranslator::visit( const CType* n )
+{
+
+}
+
+
+void CTranslator::visit( const CIfStatement* n )
+{
+
+}
+
+void CTranslator::visit( const CWhileStatement* n )
+{
+
+}
+
+void CTranslator::visit( const CComplexStatement* n )
+{
+
+}
+
+void CTranslator::visit( const CPrintStatement* n )
+{
+
+}
+
+void CTranslator::visit( const CAssignmentStatement* n )
+{
+
+}
+
+void CTranslator::visit( const CArrayAssignmentStatement* n )
+{
+
+}
+
+
+void CTranslator::visit( const CBinaryExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CIndexExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CLengthExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CMethodCallExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CIntegerExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CBooleanConstExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CIdentifierExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CThisExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CNewArrExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CNewExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CNegationExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CParenthesesExpression* n )
+{
+
+}
+
+void CTranslator::visit( const CIdentifier* n )
+{
+
 }
